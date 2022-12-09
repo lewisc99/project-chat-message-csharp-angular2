5d9967331f811ec85dafd9026049011cdb65ed40
lewisc99 <lewiscontato99@gmail.com>
2022-12-08 21:43:56 -0300

    created a new project in backend 'test project'

--- frontend/src/app/shared/auth.guard.ts
+++ frontend/src/app/shared/auth.guard.ts
@@ -21,7 +21,7 @@ export class AuthGuard implements CanActivate
         }
         else
         {
-            this.router.navigate(["/login"]);
+            this.router.navigate(["/"]);
             return false;
         }
 

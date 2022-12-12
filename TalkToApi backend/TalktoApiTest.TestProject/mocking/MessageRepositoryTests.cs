27b82bee97d1a07e126c88fcbcaacb19f1d0372b
lewisc99 <lewiscontato99@gmail.com>
2022-12-12 10:44:34 -0300

    MessageRepositoryTests get method added

--- TalkToApi backend/TalktoApiTest.TestProject/mocking/MessageRepositoryTests.cs
+++ TalkToApi backend/TalktoApiTest.TestProject/mocking/MessageRepositoryTests.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using NUnit.Framework;
+using System;
 using TalkToApiStudyTest.V1.Models;
 using TalkToApiStudyTest.V1.Repositories;
 using TalkToApiStudyTest.V1.Repositories.Contracts;
@@ -23,11 +24,44 @@ namespace TalktoApiTest.TestProject.mocking
 
             repository.Setup(method => method.Get(It.IsAny<int>())).ReturnsAsync(message);
 
-            MessageService messageService = new MessageService(repository.Object);
+            MessageService result = new MessageService(repository.Object);
 
 
-            Assert.That(messageService.Get(It.IsAny<int>()).Result, Is.EqualTo(message));
+            Assert.That(result.Get(It.IsAny<int>()).Result, Is.EqualTo(message));
 
+        }
+
+        [Test]
+        public void get_WhenCalled_ReturnNotNull()
+        {
+
+            Mock<IMessageRepository> repository = new Mock<IMessageRepository>();
+
+            Message message = new Message();
+
+            repository.Setup(method => method.Get(It.IsAny<int>())).ReturnsAsync(message);
+
+            MessageService result = new MessageService(repository.Object);
+
+
+            Assert.IsNotNull(result.Get(It.IsAny<int>()).Result);
+
+        }
+
+
+        [Test]
+        public void get_WhenCalled_ReturnException()
+        {
+            Mock<IMessageRepository> repository = new Mock<IMessageRepository>();
+
+
+            Message message = new Message();
+
+            repository.Setup(method => method.Get(It.IsAny<int>())).Throws<Exception>();
+
+            MessageService result = new MessageService(repository.Object);
+
+            Assert.ThrowsAsync<Exception>(() => result.Get(It.IsAny<int>()));
         }
 
 

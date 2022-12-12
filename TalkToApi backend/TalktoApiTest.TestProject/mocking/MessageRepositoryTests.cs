f9f5ddd8701a914a188ca551fc3b71c92aca655e
lewisc99 <lewiscontato99@gmail.com>
2022-12-12 11:02:22 -0300

    MessageRepositoryTests getMessages added

--- TalkToApi backend/TalktoApiTest.TestProject/mocking/MessageRepositoryTests.cs
+++ TalkToApi backend/TalktoApiTest.TestProject/mocking/MessageRepositoryTests.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using TalkToApiStudyTest.V1.Models;
 using TalkToApiStudyTest.V1.Repositories;
 using TalkToApiStudyTest.V1.Repositories.Contracts;
@@ -64,6 +65,92 @@ namespace TalktoApiTest.TestProject.mocking
             Assert.ThrowsAsync<Exception>(() => result.Get(It.IsAny<int>()));
         }
 
+
+
+        [Test]
+        public void getMessages_WhenCalled_ReturnListOfMessages()
+        {
+            Mock<IMessageRepository> repository = new Mock<IMessageRepository>();
+
+
+            Message message = new Message();
+
+            List<Message> messages = new List<Message>();
+
+            repository.Setup(method => method.GetMessages(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(messages);
+
+
+            MessageService result = new MessageService(repository.Object);
+
+            Assert.That(result.GetMessages(It.IsAny<string>(), It.IsAny<string>()).Result, Is.EqualTo(messages));
+     
+        }
+
+
+        [Test]
+        public void getMessages_WhenCalled_ReturnListOfMessagesNotEqualtoMessage()
+        {
+            Mock<IMessageRepository> repository = new Mock<IMessageRepository>();
+
+
+            Message message = new Message();
+
+            List<Message> messages = new List<Message>();
+
+            repository.Setup(method => method.GetMessages(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(messages);
+
+
+            MessageService result = new MessageService(repository.Object);
+
+            Assert.That(result.GetMessages(It.IsAny<string>(), It.IsAny<string>()).Result, Is.Not.EqualTo(message));
+
+        }
+
+
+
+        [Test]
+        public void getMessages_WhenCalled_ReturnException()
+        {
+            Mock<IMessageRepository> repository = new Mock<IMessageRepository>();
+
+            Message messageOne = new Message() {  Id = 1, FromId = "", ToId = "" , Text =  "Test "};
+            Message messageTwo = new Message() {  Id = 2, FromId = "", ToId = "" , Text =  "Test "};
+
+
+            List<Message> messages = new List<Message>();
+
+
+
+            messages.Add( messageOne);
+            messages.Add( messageTwo);
+
+            repository.Setup(method => method.GetMessages(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(messages);
+
+
+            MessageService result = new MessageService(repository.Object);
+
+
+            Assert.That(result.GetMessages(It.IsAny<string>(), It.IsAny<string>()).Result.Count, Is.EqualTo(2));
+
+        }
+
+        [Test]
+        public void getMessages_WhenCalled_ReturnTwoMessages()
+        {
+            Mock<IMessageRepository> repository = new Mock<IMessageRepository>();
+
+
+
+            List<Message> messages = new List<Message>();
+
+            repository.Setup(method => method.GetMessages(It.IsAny<string>(), It.IsAny<string>())).ThrowsAsync(new Exception());
+
+
+            MessageService result = new MessageService(repository.Object);
+
+
+            Assert.ThrowsAsync<Exception>(() => result.GetMessages(It.IsAny<string>(), It.IsAny<string>()));
+        }
 
 
     }

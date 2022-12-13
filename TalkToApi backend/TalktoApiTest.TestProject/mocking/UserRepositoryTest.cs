b80cad18e42b385bc7c2bcca7afeb6f29526ef6c
lewisc99 <lewiscontato99@gmail.com>
2022-12-13 10:48:17 -0300

    in userRepositoryTests create the get method tests

--- TalkToApi backend/TalktoApiTest.TestProject/mocking/UserRepositoryTest.cs
+++ TalkToApi backend/TalktoApiTest.TestProject/mocking/UserRepositoryTest.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Moq;
 using NUnit.Framework;
+using System;
 using TalkToApiStudyTest.V1.Models;
 using TalkToApiStudyTest.V1.Repositories;
+using TalkToApiStudyTest.V1.Repositories.Contracts;
+using TalkToApiStudyTest.V1.Services;
 
 namespace TalktoApiTest.mocking.TestProject
 {
@@ -26,11 +29,36 @@ namespace TalktoApiTest.mocking.TestProject
         public void get_whenCalled_ReturnUser()
         {
 
-            Mock<UserRepository> repository = new  Mock<UserRepository>();
+            Mock<IUserRepository> repository = new  Mock<IUserRepository>();
 
 
+            ApplicationUser user = new ApplicationUser() { Email = "example@gmail.com", FullName = "Lewis", Id = ""};
+            repository.Setup(m => m.Get(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(user);
+
+
+            UserService result = new UserService(repository.Object);
+
+            Assert.That(result.Get(It.IsAny<string>(), It.IsAny<string>()).Result, Is.EqualTo(user));
 
         }
+
+
+        [Test]
+        public void get_whenCalled_ThrowException()
+        {
+
+            Mock<IUserRepository> repository = new Mock<IUserRepository>();
+
+
+            repository.Setup(m => m.Get(It.IsAny<string>(), It.IsAny<string>())).ThrowsAsync(new Exception());
+
+
+            UserService result = new UserService(repository.Object);
+
+            Assert.ThrowsAsync<Exception>(() => result.Get(It.IsAny<string>(), It.IsAny<string>()));
+
+        }
+
 
         [Test]
         public void register_whenCalled_CreateANewUser()

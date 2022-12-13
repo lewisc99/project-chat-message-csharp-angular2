a3d1a15efafecc9edd86581b1f4ca0fc8da7f776
lewisc99 <lewiscontato99@gmail.com>
2022-12-13 11:16:07 -0300

    added register test in UserRepository

--- TalkToApi backend/TalktoApiTest.TestProject/mocking/UserRepositoryTests.cs
+++ TalkToApi backend/TalktoApiTest.TestProject/mocking/UserRepositoryTests.cs
@@ -11,11 +11,8 @@ namespace TalktoApiTest.mocking.TestProject
 {
 
     [TestFixture]
-   public class UserRepositoryTest
+   public class UserRepositoryTests
     {
-
-
-        private readonly Mock<UserManager<ApplicationUser>> mockUserManager;
 
 
         [SetUp]
@@ -59,12 +56,42 @@ namespace TalktoApiTest.mocking.TestProject
 
         }
 
+        [Test]
+        public void register_whenCalled_GetWithFullNameLewis()
+        {
+
+            Mock<IUserRepository> repository = new Mock<IUserRepository>();
+
+            ApplicationUser user = new ApplicationUser() { Email = "example@gmail.com", FullName = "Lewis", Id = "" };
+            repository.Setup(m => m.Get(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(user);
+
+            UserService result = new UserService(repository.Object);
+
+            Assert.That(result.Get(It.IsAny<string>(), It.IsAny<string>()).Result.FullName, Is.EqualTo("Lewis"));
+
+        }
+
+
 
         [Test]
         public void register_whenCalled_CreateANewUser()
         {
 
+            Mock<IUserRepository> repository = new Mock<IUserRepository>();
+
+            ApplicationUser user = new ApplicationUser() { Email = "example@gmail.com", FullName = "Lewis", Id = "1" };
+           
+            repository.Setup(m => m.Register(user, It.IsAny<string>()));
+
+            var result = new UserService(repository.Object);
+
+
+            Assert.That(result.Get(It.IsAny<string>()).Result, Is.EqualTo(It.IsAny<ApplicationUser>()));
+
         }
+
+
+    
 
     }
 }
